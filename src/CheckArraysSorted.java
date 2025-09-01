ad13e0a8e5fca5f15dd929a816906c77ad43a3a5
itsvinay-28 <vinay799942@gmail.com>
2025-09-02 00:16:19 +0530

    Commit

--- src/CheckArraysSorted.java
+++ src/CheckArraysSorted.java
@@ -1,10 +1,10 @@
 public class CheckArraysSorted {
     public static void main(String[] args) {
-        int []arr = {1,2,3,4,5,6};
+        int []arr = {1,2,3,4,5,9,6};
         System.out.println(Check(arr));
     }
     public static boolean Check(int[]arr){
-        for (int i = 0; i< arr.length-1;i++) {
+        for (int i = 0; i<arr.length-1;i++) {
             if(arr[i] != arr[i+1] && arr[i] > arr[i+1]){
                return false;
             }
