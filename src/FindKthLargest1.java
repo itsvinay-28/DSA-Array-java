fea5fb2c0339b9a58dd7c5d8d1468608fd37778b
itsvinay-28 <vinay799942@gmail.com>
2025-09-03 20:04:54 +0530

    Finding The largest Kth number and print in the ascending order.

---
+++ src/FindKthLargest1.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+// Finding The largest Kth number and print in the descending order.
+
+public class FindKthLargest1 {
+    static void findlargest(int []arr, int k){
+        int n = arr.length;
+        Arrays.sort(arr);
+        k  = n-k;
+        for (int i = k; i <n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int []arr = {1,2,3,5,66,8,8,99};
+        int k = 3;
+            findlargest(arr,k);
+      }
+    }
